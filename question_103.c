2b2fc3283def294f3f16b0c701eda7116f2bca47
Piyush Ralhan <Piyush.22305@stu.upes.ac.in>
2025-11-04 20:52:33 +0530

    question103

---
+++ question_103.c
@@ -0,0 +1,40 @@
+// Q103: Write a Program to take an array of integers as input, calculate the pivot index of this array. The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right. If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array. Print the leftmost pivot index. If no such index exists, print -1.
+// Q103: Write a Program to take an array of integers as input, calculate the pivot index of this array. 
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index 
+// is equal to the sum of all the numbers strictly to the index's right. 
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. 
+// This also applies to the right edge of the array. 
+// Print the leftmost pivot index. If no such index exists, print -1.
+
+#include <stdio.h>
+
+int main()
+{
+    int nums[30];
+    int n, i;
+    int totalSum = 0, leftSum = 0;
+    int pivotIndex = -1;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &nums[i]);
+        totalSum += nums[i]; 
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (leftSum == totalSum - leftSum - nums[i])
+        {
+            pivotIndex = i;  // Found leftmost pivot
+            break;
+        }
+        leftSum += nums[i]; // Add current element to left sum for next iteration
+    }
+
+    printf("%d", pivotIndex);
+    return 0;
+}
